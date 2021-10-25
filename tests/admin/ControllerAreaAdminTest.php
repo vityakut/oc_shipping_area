bd583613a53029a5e4d358303e3118a46ce44c40
vityakut <vityakut@gmail.com>
2021-10-25 10:39:33 +0300

    test

---
+++ tests/admin/ControllerAreaAdminTest.php
@@ -0,0 +1,33 @@
+<?php
+
+// require_once __DIR__ . '/../' . getenv('TEST_SUITE') . '/TestCase.php';
+
+class ControllerAreaAdminTest extends \Tests\OpenCartTest
+{
+    const MODULE_TITLE = 'area';
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $query = $this->db->query("SELECT permission from ".DB_PREFIX."user_group WHERE name = 'Administrator'");
+        $permissions = json_decode($query->row['permission'],true);
+
+        if (!in_array('extension/shipping/area', $permissions['access'])) {
+            $permissions['access'][] = 'extension/shipping/area';
+            $this->db->query("UPDATE ".DB_PREFIX."user_group SET permission='".$this->db->escape(json_encode($permissions))."' WHERE name = 'Administrator'");
+        }
+
+
+    }
+
+    public function testController()
+    {
+        $this->login('admin', 'admin');
+
+        $response = $this->dispatchAction('extension/shipping/area');
+        $this->assertRegExp('/Доставка по зонам/', $response->getOutput());
+        $this->logout();
+
+    }
+}
